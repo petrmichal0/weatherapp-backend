d99fd2095f7de92002574c8e2fc5c200b5370dff
Petr Michal <petrmichal0@gmail.com>
2024-07-29 07:03:56 +0200

    add  sameSite: "None",

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,6 +21,7 @@ const createSendToken = (user, statusCode, req, res) => {
     ),
     httpOnly: true,
     secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+    sameSite: "None",
   });
 
   // Remove password from output
