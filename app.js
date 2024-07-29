15a343a4e3fa41e100195d21a3c42b3fb0d475b9
Petr Michal <petrmichal0@gmail.com>
2024-07-30 00:27:43 +0200

    update

--- app.js
+++ app.js
@@ -24,23 +24,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://my-weatherapp-frontend-9e993cefb36f.herokuapp.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 app.options("*", cors());
 app.use(helmet());
 app.use(morgan("dev"));
