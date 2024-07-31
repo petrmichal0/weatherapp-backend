2d1106b5dac7dbc4ddf1a4bccadc72bf06b730a5
Petr Michal <petrmichal0@gmail.com>
2024-07-31 23:32:58 +0200

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -144,7 +144,6 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     const resetURL = `${req.protocol}://${req.get(
       "host"
     )}/resetpassword/${resetToken}`;
-    // const resetURL = `https://my-weatherapp-frontend-9e993cefb36f.herokuapp.com/resetpassword/${resetToken}`;
 
     await new Email(user, resetURL).sendPasswordReset();
 
@@ -163,36 +162,6 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
-
-// exports.forgotPassword = catchAsync(async (req, res, next) => {
-//   const user = await User.findOne({ email: req.body.email });
-//   if (!user) {
-//     return next(new AppError("There is no user with email address.", 404));
-//   }
-
-//   const resetToken = user.createPasswordResetToken();
-//   await user.save({ validateBeforeSave: false });
-
-//   try {
-//     const resetURL = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`; // Frontend URL
-
-//     await new Email(user, resetURL).sendPasswordReset();
-
-//     res.status(200).json({
-//       status: "success",
-//       message: "Token sent to email!",
-//     });
-//   } catch (err) {
-//     user.passwordResetToken = undefined;
-//     user.passwordResetExpires = undefined;
-//     await user.save({ validateBeforeSave: false });
-
-//     return next(
-//       new AppError("There was an error sending the email. Try again later!"),
-//       500
-//     );
-//   }
-// });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on the token
