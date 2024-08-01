c409322346ce4a8fb85c75d8fe7e3d4b12a5059a
Petr Michal <petrmichal0@gmail.com>
2024-08-01 21:26:45 +0200

    update config

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(" ")[0];
     this.url = url;
-    this.from = `WeatherApp Petr Michal <${process.env.EMAIL_FROM_SEZNAM}>`;
+    this.from = `WeatherApp Petr Michal <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
